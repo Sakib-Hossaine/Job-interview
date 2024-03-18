e1a79e45f1ecf06a90b807a9621f590203469a0f
Md. Sakib Hossaine <116342600+Sakib-Hossaine@users.noreply.github.com>
2024-03-18 16:54:40 +0600

    Add files via upload

---
+++ leet code set problem/leetcodw.cpp
@@ -0,0 +1,7 @@
+//https://leetcode.com/problems/contains-duplicate/description/
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+  return nums.size()>set<int>(nums.begin(),nums.end()).size();
+  }
+};
